3e931f19b176446610f6538d20629f2d526155fb
Your Name <miaoamirror@gmail.com>
2021-11-24 16:01:25 +0800

    测试onSaveInstanceState(Bundle)

--- ActivityLifeCycleTest/app/src/main/java/miao/kmirror/activitylifecycletest/MainActivity.java
+++ ActivityLifeCycleTest/app/src/main/java/miao/kmirror/activitylifecycletest/MainActivity.java
@@ -18,6 +18,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_main);
+        if (savedInstanceState != null) {
+            Log.d(TAG, "miao");
+            String tempData = savedInstanceState.getString("data_key");
+            Log.d("NormalActivity", tempData);
+        }
         Button startNormalActivity = (Button) findViewById(R.id.start_normal_activity);
         Button startDialogActivity = (Button) findViewById(R.id.start_dialog_activity);
         startNormalActivity.setOnClickListener(new View.OnClickListener() {
@@ -35,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
 
     @Override
     protected void onStart() {
@@ -72,4 +78,10 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onRestart");
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        String tempData = "This is some temp data!";
+        outState.putString("data_key", tempData);
+    }
 }
--- ActivityLifeCycleTest/app/src/main/java/miao/kmirror/activitylifecycletest/NormalActivity.java
+++ ActivityLifeCycleTest/app/src/main/java/miao/kmirror/activitylifecycletest/NormalActivity.java
@@ -2,13 +2,36 @@ package miao.kmirror.activitylifecycletest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class NormalActivity extends AppCompatActivity {
-
+    public static final String TAG = "NormalActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_normal);
+        Log.d(TAG, "Normal create");
+
+
+        Button killButton = (Button) findViewById(R.id.kill_normal);
+        killButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        String tempData = "This is some temp data!";
+        outState.putString("data_key", tempData);
+        Log.d(TAG, "miaomiaomiao");
+    }
+
 }
