4e76e847c61df84b7af2bfa7e9666b76b29309f8
Kmirror <Kmirror@outlook.com>
2021-11-24 22:30:40 +0800

    ProgressBar 按钮切换是否可视 （View.VISIBLE, View.INVISIBLE, View.GONE）

--- UIWidgetTest/app/src/main/java/miao/kmirror/uiwidgettest/MainActivity.java
+++ UIWidgetTest/app/src/main/java/miao/kmirror/uiwidgettest/MainActivity.java
@@ -8,11 +8,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText editText;
     private ImageView imageView;
+    private ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,15 +23,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button button = (Button) findViewById(R.id.button);
         editText = (EditText) findViewById(R.id.edit_text);
         imageView = (ImageView) findViewById(R.id.image_view);
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
         button.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.button:
                 // 此处添加逻辑
-                imageView.setImageResource(R.drawable.img_2);
+                if (progressBar.getVisibility() == View.GONE) {
+                    progressBar.setVisibility(View.VISIBLE);
+                } else {
+                    progressBar.setVisibility(View.GONE);
+                }
                 break;
             default:
                 break;
