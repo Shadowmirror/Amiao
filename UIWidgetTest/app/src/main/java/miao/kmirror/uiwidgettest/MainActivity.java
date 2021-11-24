5e28c98f20f79c736900e73807cb3b30870cd501
Kmirror <Kmirror@outlook.com>
2021-11-24 22:41:08 +0800

    AlertDialog相关内容

--- UIWidgetTest/app/src/main/java/miao/kmirror/uiwidgettest/MainActivity.java
+++ UIWidgetTest/app/src/main/java/miao/kmirror/uiwidgettest/MainActivity.java
@@ -1,7 +1,9 @@
 package miao.kmirror.uiwidgettest;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
@@ -32,9 +34,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()) {
             case R.id.button:
                 // 此处添加逻辑
-                int progress = progressBar.getProgress();
-                progress = progress + 10;
-                progressBar.setProgress(progress);
+                // 通过AlterDialog.Builder创建一个AlertDialog的实例
+                AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                // 设置标题
+                dialog.setTitle("This is a Dialog");
+                // 设置内容
+                dialog.setMessage("这是一些重要的东西");
+                // 是否可以使用Back键关闭对话框
+                dialog.setCancelable(false);
+                // 设置确认按钮的点击事件
+                dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                // 设置取消按钮的点击事件
+                dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                // 调用show()方法显示
+                dialog.show();
                 break;
             default:
                 break;
