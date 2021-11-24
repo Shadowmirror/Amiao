b5913d3b3b0bd7a1b35cfdbcddb327d7bde4f28c
Your Name <miaoamirror@gmail.com>
2021-11-24 17:51:44 +0800

    Android UI 初体验

--- ActivityTest/app/src/main/java/miao/kmirror/activitytest/ThirdActivity.java
+++ ActivityTest/app/src/main/java/miao/kmirror/activitytest/ThirdActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class ThirdActivity extends BaseActivity {
 
@@ -13,5 +15,12 @@ public class ThirdActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "Task id is " + getTaskId());
         setContentView(R.layout.activity_third);
+        Button button3 = (Button) findViewById(R.id.button_3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ActivityCollector.finishAll();
+            }
+        });
     }
 }
