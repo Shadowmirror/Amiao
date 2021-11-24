56a3f4dccaf37e1e6f320c7af3194f8ffbec61b4
Kmirror <Kmirror@outlook.com>
2021-11-24 22:22:07 +0800

    ImageView相关

--- UIWidgetTest/app/src/main/java/miao/kmirror/uiwidgettest/MainActivity.java
+++ UIWidgetTest/app/src/main/java/miao/kmirror/uiwidgettest/MainActivity.java
@@ -2,20 +2,24 @@ package miao.kmirror.uiwidgettest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText editText;
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = (Button) findViewById(R.id.button);
         editText = (EditText) findViewById(R.id.edit_text);
+        imageView = (ImageView) findViewById(R.id.image_view);
         button.setOnClickListener(this);
     }
 
@@ -24,8 +28,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()){
             case R.id.button:
                 // 此处添加逻辑
-                String inputText = editText.getText().toString();
-                Toast.makeText(MainActivity.this, inputText, Toast.LENGTH_LONG).show();
+                imageView.setImageResource(R.drawable.img_2);
                 break;
             default:
                 break;
