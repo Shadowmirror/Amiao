65cd8e63b3d1b6da319da4dc4cd01260196983b6
Kmirror <Kmirror@outlook.com>
2021-11-25 11:23:53 +0800

    ListView 使用项目初创建

--- UICustomViews/app/src/main/java/miao/kmirror/uicustomviews/TitleLayout.java
+++ UICustomViews/app/src/main/java/miao/kmirror/uicustomviews/TitleLayout.java
@@ -1,10 +1,14 @@
 package miao.kmirror.uicustomviews;
 
+import android.app.Activity;
 import android.content.Context;
 import android.text.Layout;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -21,5 +25,23 @@ public class TitleLayout extends LinearLayout {
         //  第二个参数是给加载好的布局再添加一个父布局，这里我们想要指定 TitleLayout
         //  于是直接传入 this
         LayoutInflater.from(context).inflate(R.layout.title, this);
+        Button titleBack = (Button) findViewById(R.id.title_back);
+        Button titleEdit = (Button) findViewById(R.id.title_edit);
+        titleBack.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((Activity) getContext()).finish();
+            }
+        });
+        titleEdit.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(),
+                        "You clicked Eidt button",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+
     }
 }
