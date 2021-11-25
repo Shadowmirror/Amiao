360cd0a821b00471a31078902373ebff17a3ae87
Kmirror <Kmirror@outlook.com>
2021-11-25 14:57:51 +0800

    定制界面的 ListView 运行效果 更新 MarkDown 内容

--- ListViewTest/app/src/main/java/miao/kmirror/listviewtest/FruitAdapter.java
+++ ListViewTest/app/src/main/java/miao/kmirror/listviewtest/FruitAdapter.java
@@ -2,6 +2,7 @@ package miao.kmirror.listviewtest;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,16 +18,23 @@ import java.util.List;
 public class FruitAdapter extends ArrayAdapter<Fruit> {
     private int resourceId;
 
+    // FruitAdapter 重写了父类的一组构造函数，用于将上下文、ListView 子项布局的 id 和数据都传递进来。
     public FruitAdapter(@NonNull Context context, int resource, @NonNull List<Fruit> objects) {
         super(context, resource, objects);
         resourceId = resource;
     }
 
+    // 重写 getView() 方法，这个方法在每个子项被滚动到屏幕内的时候会被调用。在 getView() 方法中，首先通过 getItem()
+    // 方法得到当前项的 Fruit 实例，然后使用 LayoutInflater 来为这个子项加载我们传入的布局。
+
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         Fruit fruit = getItem(position);
-        @SuppressLint("ViewHolder") View view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
+        // 这里 LayoutInflater 的 inflate() 方法接收 3 个参数，前两个参数我们已经知道什么意思了，
+        // 第三个参数指定成 false，表示只让我们在父布局中声明的 layout 属性生效，但不会为这个 View 添加父布局，
+        // 因为一旦 View 有了父布局之后，它就不能再添加到 ListView 中了。
+        View view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
         ImageView imageView = view.findViewById(R.id.fruit_image);
         TextView textView =  view.findViewById(R.id.fruit_name);
         imageView.setImageResource(fruit.getImageId());
