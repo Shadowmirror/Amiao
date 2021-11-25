b011bcc8f3de6ea76babf76fcc3b905abe86c1c7
Kmirror <Kmirror@outlook.com>
2021-11-25 22:51:02 +0800

    Fragment 动态添加 Fragment

--- FragmentTest/app/src/main/java/miao/kmirror/fragmenttest/MainActivity.java
+++ FragmentTest/app/src/main/java/miao/kmirror/fragmenttest/MainActivity.java
@@ -1,14 +1,46 @@
 package miao.kmirror.fragmenttest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button =(Button) findViewById(R.id.button);
+        Button button2 = (Button) findViewById(R.id.button_2);
+        button.setOnClickListener(this);
+        button2.setOnClickListener(this);
+
+        replaceFragment(new RightFragment());
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button:
+                replaceFragment(new AnotherRightFragment());
+                break;
+            case R.id.button_2:
+                replaceFragment(new RightFragment());
+            default:
+                break;
+        }
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        // 本质上是 remove R.id.right_layout 中所有的 fragment，同时再添加传入的 fragment
+        transaction.replace(R.id.right_layout, fragment);
+        transaction.commit();
     }
 }
