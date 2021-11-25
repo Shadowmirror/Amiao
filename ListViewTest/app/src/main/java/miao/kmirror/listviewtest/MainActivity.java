b4f3a859088e15d1155ca90db05e9756985c47d0
Kmirror <Kmirror@outlook.com>
2021-11-25 15:32:14 +0800

    ListView 的点击事件

--- ListViewTest/app/src/main/java/miao/kmirror/listviewtest/MainActivity.java
+++ ListViewTest/app/src/main/java/miao/kmirror/listviewtest/MainActivity.java
@@ -3,8 +3,11 @@ package miao.kmirror.listviewtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,13 +24,20 @@ public class MainActivity extends AppCompatActivity {
         FruitAdapter adapter = new FruitAdapter(MainActivity.this, R.layout.fruit_item, fruitList);
         ListView listView = (ListView) findViewById(R.id.list_view);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Fruit fruit = fruitList.get(position);
+                Toast.makeText(MainActivity.this, fruit.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void initFruits() {
         for (int i = 0; i < 2; i++) {
             Fruit apple = new Fruit("Apple", R.drawable.apple_pic);
             fruitList.add(apple);
-            Fruit banana = new Fruit("Apple", R.drawable.banana_pic);
+            Fruit banana = new Fruit("Banana", R.drawable.banana_pic);
             fruitList.add(banana);
             Fruit orange = new Fruit("Orange", R.drawable.orange_pic);
             fruitList.add(orange);
