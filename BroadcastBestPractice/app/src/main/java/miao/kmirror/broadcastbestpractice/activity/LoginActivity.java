e39706ec0e09031cc24db410ff5a6df4eb412cad
Kmirror <Kmirror@outlook.com>
2021-11-29 20:38:36 +0800

    实现强制下线功能 Finished

--- BroadcastBestPractice/app/src/main/java/miao/kmirror/broadcastbestpractice/activity/LoginActivity.java
+++ BroadcastBestPractice/app/src/main/java/miao/kmirror/broadcastbestpractice/activity/LoginActivity.java
@@ -3,12 +3,17 @@ package miao.kmirror.broadcastbestpractice.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.prefs.PreferenceChangeEvent;
 
 import miao.kmirror.broadcastbestpractice.R;
 
@@ -16,28 +21,53 @@ public class LoginActivity extends BaseActivity {
 
     public static final String TAG = "LoginActivity";
 
+    private SharedPreferences preferences;
+
+    private SharedPreferences.Editor editor;
+
     private EditText accountEdit;
 
     private EditText passwordEdit;
 
     private Button login;
 
+    private CheckBox rememberPass;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
         accountEdit = (EditText) findViewById(R.id.account);
         passwordEdit = (EditText) findViewById(R.id.password);
         login = (Button) findViewById(R.id.login);
+        rememberPass = (CheckBox) findViewById(R.id.remember_pass);
+        boolean isRemember = preferences.getBoolean("remember_password", false);
+        if(isRemember){
+            // 将账号密码都设置到文本框中
+            String account = preferences.getString("account", "");
+            String password = preferences.getString("password", "");
+            accountEdit.setText(account);
+            passwordEdit.setText(password);
+            rememberPass.setChecked(true);
+        }
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String account = accountEdit.getText().toString();
-                Log.d(TAG, account);
                 String password = passwordEdit.getText().toString();
-                Log.d(TAG, password);
                 // 如果账号是 admin 且密码是 123456，就认为登录成功
                 if(account.equals("admin") && password.equals("123456")){
+                    editor = preferences.edit();
+                    // 检查复选框是否选中
+                    if(rememberPass.isChecked()){
+                        editor.putBoolean("remember_password", true);
+                        editor.putString("account", account);
+                        editor.putString("password", password);
+                    } else{
+                        editor.clear();
+                    }
+                    editor.apply();
                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                     startActivity(intent);
                     finish();
