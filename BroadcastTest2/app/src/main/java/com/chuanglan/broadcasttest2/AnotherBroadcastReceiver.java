58bb021b83d0bd5cf0ec28f43712ea4b4d9b9109
Kmirror <Kmirror@outlook.com>
2021-11-29 14:22:25 +0800

    验证不同应用程序间的广播通讯

---
+++ BroadcastTest2/app/src/main/java/com/chuanglan/broadcasttest2/AnotherBroadcastReceiver.java
@@ -0,0 +1,14 @@
+package com.chuanglan.broadcasttest2;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class AnotherBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Recevied in AnotherBroadcastReceiver", Toast.LENGTH_SHORT).show();
+    }
+}
