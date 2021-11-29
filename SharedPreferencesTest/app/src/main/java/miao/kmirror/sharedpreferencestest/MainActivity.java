bc6434c7b5a59ebed23ef4b698b84c819414536b
Kmirror <Kmirror@outlook.com>
2021-11-29 20:05:03 +0800

    SharedPreferencesTest —— 从 SharedPreferences 中获取数据

--- SharedPreferencesTest/app/src/main/java/miao/kmirror/sharedpreferencestest/MainActivity.java
+++ SharedPreferencesTest/app/src/main/java/miao/kmirror/sharedpreferencestest/MainActivity.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +26,20 @@ public class MainActivity extends AppCompatActivity {
                 editor.apply();
             }
         });
+
+        Button restoreData = (Button) findViewById(R.id.restore_data);
+        restoreData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences pref = getSharedPreferences("data", MODE_PRIVATE);
+                String name = pref.getString("name", "");
+                int age = pref.getInt("age", 0);
+                boolean married = pref.getBoolean("married", false);
+                Log.d(TAG, "name is " + name);
+                Log.d(TAG, "age is " + age);
+                Log.d(TAG, "married is " + married);
+            }
+        });
+
     }
 }
