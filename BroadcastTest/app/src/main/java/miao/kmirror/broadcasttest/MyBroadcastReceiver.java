63680990b01b509af29125eb208906016ba39c7e
Kmirror <Kmirror@outlook.com>
2021-11-29 15:11:38 +0800

    有序广播的截断

--- BroadcastTest/app/src/main/java/miao/kmirror/broadcasttest/MyBroadcastReceiver.java
+++ BroadcastTest/app/src/main/java/miao/kmirror/broadcasttest/MyBroadcastReceiver.java
@@ -10,5 +10,6 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Toast.makeText(context, "这是接收自定义广播的接收器", Toast.LENGTH_SHORT).show();
+        abortBroadcast();
     }
 }
