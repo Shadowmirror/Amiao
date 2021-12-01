80a0d58670dff4b69e2f5922bb1fb6f3512cad6a
Kmirror <Kmirror@outlook.com>
2021-12-01 11:15:02 +0800

    ProviderTest -- Uri newUri = getContentResolver().insert(uri, values); 无法获取 newUri 待解决

--- ProviderTest/app/src/main/java/miao/kmirror/providertest/MainActivity.java
+++ ProviderTest/app/src/main/java/miao/kmirror/providertest/MainActivity.java
@@ -2,13 +2,87 @@ package miao.kmirror.providertest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String newId;
+
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button addData = (Button) findViewById(R.id.add_data);
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 添加数据
+                Uri uri = Uri.parse("content://miao.kmirror.databasetest.provider/book");
+                Log.i(TAG, "uri = " + uri);
+                ContentValues values = new ContentValues();
+                values.put("name", "A Clash of Kings");
+                values.put("author", "George Martin");
+                values.put("pages", 1040);
+                values.put("price", 22.85);
+                Uri newUri = getContentResolver().insert(uri, values);
+                Log.i(TAG, "newUri = " + newUri);
+                newId = newUri.getPathSegments().get(1);
+            }
+        });
+
+        Button queryData = findViewById(R.id.query_data);
+        queryData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("content://miao.kmirror.databasetest.provider/book");
+                Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+                if (cursor != null) {
+                    while (cursor.moveToNext()) {
+                        @SuppressLint("Range") String name = cursor.getString(cursor.getColumnIndex("name"));
+                        @SuppressLint("Range") String author = cursor.getString(cursor.getColumnIndex("author"));
+                        @SuppressLint("Range") int pages = cursor.getInt(cursor.getColumnIndex("pages"));
+                        @SuppressLint("Range") double price = cursor.getDouble(cursor.getColumnIndex("price"));
+                        Log.d(TAG, "book name is " + name);
+                        Log.d(TAG, "book author is " + author);
+                        Log.d(TAG, "book pages is " + pages);
+                        Log.d(TAG, "book price is " + price);
+                    }
+                    cursor.close();
+                }
+            }
+        });
+
+        Button updateData = findViewById(R.id.update_data);
+        updateData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 更新数据
+                Uri uri = Uri.parse("content://miao.kmirror.databasetest.provider/book" + newId);
+                ContentValues values = new ContentValues();
+                values.put("name", "A storm of Swords");
+                values.put("pages", 1216);
+                values.put("price", 24.05);
+                getContentResolver().update(uri, values, null, null);
+            }
+        });
+
+        Button deleteData = findViewById(R.id.delete_data);
+        deleteData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 删除数据
+                Uri uri = Uri.parse("content://miao.kmirror.databasetest.provider/book" + newId);
+                getContentResolver().delete(uri, null,null  );
+            }
+        });
     }
 }
