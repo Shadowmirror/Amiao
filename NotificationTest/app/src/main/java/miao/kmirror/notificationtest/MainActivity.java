9576fb1df6356f4f16771e5806b828e19561d9d9
Kmirror <Kmirror@outlook.com>
2021-12-01 13:46:08 +0800

    NotificationTest -- 通知的基本用法

--- NotificationTest/app/src/main/java/miao/kmirror/notificationtest/MainActivity.java
+++ NotificationTest/app/src/main/java/miao/kmirror/notificationtest/MainActivity.java
@@ -1,14 +1,54 @@
 package miao.kmirror.notificationtest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * @author Kmirror
+ */
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button sendNotice = findViewById(R.id.send_notice);
+        sendNotice.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.send_notice:
+                Intent intent = new Intent(this, NotificationActivity.class);
+                PendingIntent pi = PendingIntent.getActivity(this, 0, intent, 0);
+                NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+                // ContentIntent 点击通知可以进入活动
+                // setAutoCancel 点击后通知自动取消
+
+                Notification notification = new NotificationCompat.Builder(this)
+                        .setContentTitle("This is content title")
+                        .setContentText("This is content text")
+                        .setWhen(System.currentTimeMillis())
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                        .setContentIntent(pi)
+                        .setAutoCancel(true)
+                        .build();
+                manager.notify(1, notification);
+                break;
+            default:
+                break;
+        }
     }
 }
