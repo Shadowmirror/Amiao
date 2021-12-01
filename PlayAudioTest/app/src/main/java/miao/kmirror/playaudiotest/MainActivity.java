473a4926177a38b59522ba3dd28b1900a02fcc29
Kmirror <Kmirror@outlook.com>
2021-12-01 18:33:25 +0800

    PlayAudioTest -- 待解决

--- PlayAudioTest/app/src/main/java/miao/kmirror/playaudiotest/MainActivity.java
+++ PlayAudioTest/app/src/main/java/miao/kmirror/playaudiotest/MainActivity.java
@@ -1,14 +1,96 @@
 package miao.kmirror.playaudiotest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author EDY
+ */
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    public static final String TAG = "MainActivity";
+
+    private MediaPlayer mediaPlayer = new MediaPlayer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button play = findViewById(R.id.play);
+        Button pause = findViewById(R.id.pause);
+        Button stop = findViewById(R.id.stop);
+        try {
+            initMediaPlayer();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        play.setOnClickListener(this);
+        pause.setOnClickListener(this);
+        stop.setOnClickListener(this);
+
+
+    }
+
+    private void initMediaPlayer() throws IOException {
+        AssetManager assets = getAssets();
+        AssetFileDescriptor fd = assets.openFd("music.mp3");
+        mediaPlayer.setDataSource(fd.getFileDescriptor(), fd.getStartOffset(), fd.getLength());
+        mediaPlayer.prepare();
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.play:
+                if(!mediaPlayer.isPlaying()){
+                    mediaPlayer.start();
+                }
+                break;
+            case R.id.pause:
+                if(mediaPlayer.isPlaying()){
+                    mediaPlayer.pause();
+                }
+                break;
+            case R.id.stop:
+                if(mediaPlayer.isPlaying()){
+                    // 停止播放
+                    mediaPlayer.reset();
+                    try {
+                        initMediaPlayer();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mediaPlayer != null){
+            mediaPlayer.stop();
+            mediaPlayer.release();
+        }
     }
 }
