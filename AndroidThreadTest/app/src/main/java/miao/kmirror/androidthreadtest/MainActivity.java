39f199652810fd9fca7eb40a8b233bbd19e0b42c
Kmirror <Kmirror@outlook.com>
2021-12-02 17:11:54 +0800

    AndroidThreadTest

---
+++ AndroidThreadTest/app/src/main/java/miao/kmirror/androidthreadtest/MainActivity.java
@@ -0,0 +1,37 @@
+package miao.kmirror.androidthreadtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button changeText = findViewById(R.id.change_text);
+        textView = findViewById(R.id.text);
+        changeText.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.change_text:
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        textView.setText("Nice to meet you");
+                    }
+                }).start();
+            default:
+                break;
+        }
+    }
+}
