7ce0fbe1f0ab22937506d5409e9ee62b8c8b5eb9
Kmirror <Kmirror@outlook.com>
2021-12-02 10:09:59 +0800

    PlayAudioTest -- 可用版

--- PlayAudioTest/app/src/main/java/miao/kmirror/playaudiotest/MainActivity.java
+++ PlayAudioTest/app/src/main/java/miao/kmirror/playaudiotest/MainActivity.java
@@ -1,15 +1,20 @@
 package miao.kmirror.playaudiotest;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
+import android.media.AudioManager;
+import android.media.MediaParser;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
@@ -23,72 +28,66 @@ import java.io.IOException;
 /**
  * @author EDY
  */
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     public static final String TAG = "MainActivity";
 
     private MediaPlayer mediaPlayer = new MediaPlayer();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button play = findViewById(R.id.play);
         Button pause = findViewById(R.id.pause);
         Button stop = findViewById(R.id.stop);
-        try {
-            initMediaPlayer();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
         play.setOnClickListener(this);
         pause.setOnClickListener(this);
         stop.setOnClickListener(this);
-
-
-    }
-
-    private void initMediaPlayer() throws IOException {
-        AssetManager assets = getAssets();
-        AssetFileDescriptor fd = assets.openFd("music.mp3");
-        mediaPlayer.setDataSource(fd.getFileDescriptor(), fd.getStartOffset(), fd.getLength());
-        mediaPlayer.prepare();
+        initMediaPlayer();
     }
 
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.play:
-                if(!mediaPlayer.isPlaying()){
+                if (!mediaPlayer.isPlaying()) {
                     mediaPlayer.start();
                 }
                 break;
+
             case R.id.pause:
-                if(mediaPlayer.isPlaying()){
+                if (mediaPlayer.isPlaying()) {
                     mediaPlayer.pause();
                 }
                 break;
             case R.id.stop:
                 if(mediaPlayer.isPlaying()){
-                    // 停止播放
                     mediaPlayer.reset();
-                    try {
-                        initMediaPlayer();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                    initMediaPlayer();
                 }
                 break;
             default:
-                break;
+
+        }
+    }
+
+    private void initMediaPlayer() {
+        Uri uri = Uri.parse("android.resource://" + getApplicationContext().getPackageName() + "/" + R.raw.music);
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        try {
+            mediaPlayer.setDataSource(this, uri);
+            mediaPlayer.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if(mediaPlayer != null){
+        if (mediaPlayer != null) {
             mediaPlayer.stop();
             mediaPlayer.release();
         }
