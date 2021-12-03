ef48ae31975b6915ad44f8df8e76a1f4f76cf250
Kmirror <Kmirror@outlook.com>
2021-12-03 09:36:46 +0800

    Service 的生命周期

--- ServiceTest/app/src/main/java/miao/kmirror/servicetest/MainActivity.java
+++ ServiceTest/app/src/main/java/miao/kmirror/servicetest/MainActivity.java
@@ -7,10 +7,13 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final String TAG = "MainActivity";
 
     private MyService.DownloadBinder downloadBinder;
 
@@ -52,6 +55,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.stop_service:
                 Intent stopIntent = new Intent(this, MyService.class);
+                Log.i(TAG, "onClick: 我进来了");
                 stopService(stopIntent);
                 break;
             case R.id.bind_service:
