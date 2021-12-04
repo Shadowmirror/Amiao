d05a4481656ba9e477ee73ee979c99dc5cba3c10
Kmirror <Kmirror@outlook.com>
2021-12-04 11:45:19 +0800

    滑动菜单 -- 显示 HomeAsUp

--- MaterialTest/app/src/main/java/miao/kmirror/materialtest/MainActivity.java
+++ MaterialTest/app/src/main/java/miao/kmirror/materialtest/MainActivity.java
@@ -1,8 +1,11 @@
 package miao.kmirror.materialtest;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,22 +14,48 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        /**
+         * 调用 getSupportActionBar() 方法得到了 ActionBar 的实例，虽然这个 ActionBar 的具体实现是由 Toolbar
+         * 来完成的。接着调用 ActionBar 的 setDisplayHomeAsUpEnabled() 方法让导航按钮显示出来，
+         * 又调用 setHomeAsUpIndicator() 方法来设置一个导航按钮图标。
+         * 实际上，Toolbar 最左侧的这个按钮就叫做 HomeAsUp 按钮，它默认的图标是一个返回的箭头，含义是放回上一个活动。
+         * 很明显，这里我们将它的默认样式和作用都进行了修改
+         * */
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        }
+
     }
 
-    public boolean onCreateOptionsMenu(Menu menu){
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.toolbar, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch(item.getItemId()){
+        switch (item.getItemId()) {
+            /**
+             * HomeAsUp 按钮的 id 永远都是 android.R.id.home。然后调用 DrawerLayout 的 openDrawer()
+             * 方法将滑动菜单展示出来，注意 openDrawer() 方法要求传入一个 Gravity 参数，
+             * 为了保证这里的行为和 XML 中定义的一致，我们传入了 GravityCompat.START
+             * */
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
             case R.id.backup:
                 Toast.makeText(this, "You clicked Backup", Toast.LENGTH_SHORT).show();
                 break;
