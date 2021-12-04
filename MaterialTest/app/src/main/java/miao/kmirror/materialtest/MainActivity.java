be0618b51c08ceb6f2359fe67b6919ab9eb41819
Kmirror <Kmirror@outlook.com>
2021-12-04 14:07:01 +0800

    滑动菜单 --  FloatingActionButton

--- MaterialTest/app/src/main/java/miao/kmirror/materialtest/MainActivity.java
+++ MaterialTest/app/src/main/java/miao/kmirror/materialtest/MainActivity.java
@@ -10,8 +10,10 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -46,6 +48,14 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 mDrawerLayout.closeDrawers();
                 return true;
+            }
+        });
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "FloatingActionButton clicked", Toast.LENGTH_SHORT).show();
             }
         });
 
